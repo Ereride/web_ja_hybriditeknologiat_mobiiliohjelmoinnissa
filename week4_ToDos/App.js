b23936c98d0bb7552797f66be4df148fc9dd4b70
Ereride <143874893+Ereride@users.noreply.github.com>
2024-09-18 21:01:29 +0300

    todo list
    add, list and delete

--- week4_ToDos/App.js
+++ week4_ToDos/App.js
@@ -1,20 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { StyleSheet, SafeAreaView, FlatList, Text } from 'react-native';
+import Constants from 'expo-constants';
+import Row from './components/Row';
+import Add from './components/Add';
+import uuid from 'react-native-uuid';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+const STORAGE_KEY ='@items_key'
 
 export default function App() {
+ const [data, setData] =useState([])
+ const [selectedId, setSelectedId] = useState(null)
+
+  useEffect(() => {
+    //AsyncStorage.clear()
+    getData()
+  }, [])
+
+ useEffect(() => {
+  storeData(data)
+ }, [data])
+
+ const add = useCallback((name) => {
+  const newItem = {
+    id: uuid.v4(),
+    name: name
+  }
+  const tempData = [...data,newItem]
+  setData(tempData)
+ }, [data])
+
+ 
+ const select = (id) => {
+  setSelectedId(id)
+ }
+
+ const getData = async() => {
+  try {
+    const value = await AsyncStorage.getItem(STORAGE_KEY)
+    const json = JSON.parse(value)
+    if (json === null) {
+      json = []
+    }
+    setData(json)
+  } catch (ex) {
+    console.log(ex)
+  }
+ }
+
+ const storeData = async(value) => {
+  try {
+    const json = JSON.stringify(value)
+    await AsyncStorage.setItem(STORAGE_KEY,json)
+  } catch (ex) {
+    console.log(ex)
+  }
+ }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Tehtävä lista</Text>
+      <Add add= {add} setData={setData}/>
+      <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      extraData={selectedId}
+      renderItem={({item}) => (
+        <Row 
+        item={item}
+        selectedId={selectedId}
+        select={select}
+        data = {data}
+        setData = {setData}
+        />
+        )}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: Constants.statusBarHeight,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingLeft: 8,
+    paddingRight: 8,
+    
   },
+
+  title: {
+    paddingTop: 20,
+    paddingBottom: 20,
+    fontSize: 20,
+    textAlign: 'center'
+}
 });
---
+++ week4_ToDos/components/Add.js
@@ -0,0 +1,57 @@
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+
+export default function Add({add}) {
+    const [task, setTask] = useState('')
+    
+    const save = () => {
+        add(task)
+        setTask('')
+    }
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style ={styles.form} 
+        value={task} 
+        placeholder='Tänään tehtävää' 
+        onChangeText={text => setTask(text)}
+      />
+      <TouchableOpacity style={styles.button} onPress={save}>
+        <Text style={styles.buttonText}>Save</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flexDirection: 'row', 
+      justifyContent: 'space-between', 
+      alignItems: 'center', 
+      marginBottom: 16,
+    },
+  
+    form: {
+      flex: 1,
+      fontSize: 18,
+      padding: 10,
+      borderWidth: 1,
+      borderColor: 'gray',
+      marginRight: 10, 
+    },
+  
+    button: {
+      backgroundColor: '#007bff',
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      borderRadius: 5,
+      justifyContent: 'center', 
+    },
+  
+    buttonText: {
+      color: 'white',
+      fontSize: 16,
+      textAlign: 'center', 
+    },
+  });
